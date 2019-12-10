173d57bd09b68a5cf2ad1e70f6ca63ec59518fc1
Karl Nicholas <karl.nicholas@outlook.com>
2019-12-09 21:33:48 -0700

    Cleanup @bean

--- TransformationService/src/main/java/com/github/karlnicholas/rs/transformationservice/TransformationServiceApplication.java
+++ TransformationService/src/main/java/com/github/karlnicholas/rs/transformationservice/TransformationServiceApplication.java
@@ -2,10 +2,7 @@ package com.github.karlnicholas.rs.transformationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.config.EnableWebFlux;
-
-import com.github.karlnicholas.rs.serviceclient.AccountWebClient;
 
 @SpringBootApplication(scanBasePackages = {"com.github.karlnicholas.rs"})
 @EnableWebFlux
@@ -13,10 +10,5 @@ public class TransformationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransformationServiceApplication.class, args);
-	}
-
-	@Bean
-	AccountWebClient getAccountWebClient() {
-		return new AccountWebClient();
 	}
 }
